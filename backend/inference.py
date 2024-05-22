d2aa7365524b11a553ac20d4645cafda556a0916
B Rahul <rahul20bollisetty@gmail.com>
2024-05-22 11:31:07 +0530

    jenkinsfile updated

--- backend/inference.py
+++ backend/inference.py
@@ -36,7 +36,7 @@ loaded_model = CustomCNN(num_classes)
 
 # Load the model's state dictionary and map it to CPU
 model_path = os.getenv('MODEL_PATH', '/mnt/data/best_model.pth')  # Default value
-loaded_model.load_state_dict(torch.load("/mnt/data/best_model.pth", map_location=torch.device('cpu')))
+loaded_model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
 
 # Set the model to evaluation mode
 loaded_model.eval()
